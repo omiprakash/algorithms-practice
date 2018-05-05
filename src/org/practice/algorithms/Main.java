227727ae904b36b595de59333bb0dade623cb631
omi <omiprakash07_jan@hotmail.com>
2018-05-06 01:55:15 +0530

    welcome commit

---
+++ src/org/practice/algorithms/Main.java
@@ -0,0 +1,9 @@
+package org.practice.algorithms;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code heres
+        System.out.println("Welcome to algorithms");
+    }
+}
